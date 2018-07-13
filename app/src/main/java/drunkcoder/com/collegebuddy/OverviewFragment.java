a85aa2e5e916f012ee06a3d3006cb57962bdd6c7
akraturi <mhraturi@gmail.com>
2018-07-13 18:42:28 +0530

    Floating action button and OverviewFragment added! No more real work on master!

---
+++ app/src/main/java/drunkcoder/com/collegebuddy/OverviewFragment.java
@@ -0,0 +1,11 @@
+package drunkcoder.com.collegebuddy;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * FRAGMENT DISPLAYS THE OVERVIEW OF THE USER SCHEDUAL, ATTENDANCE , MARKS  AND REMINDERS
+ *
+ */
+
+public class OverviewFragment extends Fragment {
+}
