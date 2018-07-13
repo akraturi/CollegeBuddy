10dc0fa248de192993406082cf61b78e790638cf
akraturi <mhraturi@gmail.com>
2018-07-14 01:31:04 +0530

    OverviewFragment layout stucture ready!

--- app/src/main/java/drunkcoder/com/collegebuddy/OverviewFragment.java
+++ app/src/main/java/drunkcoder/com/collegebuddy/OverviewFragment.java
@@ -1,11 +1,31 @@
 package drunkcoder.com.collegebuddy;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 /**
  * FRAGMENT DISPLAYS THE OVERVIEW OF THE USER SCHEDUAL, ATTENDANCE , MARKS  AND REMINDERS
  *
  */
-
 public class OverviewFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_overview,container,false);
+
+        return view;
+    }
+
+    public static OverviewFragment newInstance()
+    {
+        return new OverviewFragment();
+    }
 }
