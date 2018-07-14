6ec0ee7b49caa0cb8fff00571bdb7c42053cb898
akraturi <mhraturi@gmail.com>
2018-07-14 14:01:20 +0530

    Details activity added!

---
+++ app/src/main/java/drunkcoder/com/collegebuddy/DetailsActivity.java
@@ -0,0 +1,94 @@
+package drunkcoder.com.collegebuddy;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ImageView headerImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        // up button
+         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+         headerImageView=findViewById(R.id.appbar_header_image);
+         int selectedCategory=getIntent().getIntExtra("selectedCategory",0);
+         setHeaderImage(selectedCategory);
+
+         addFragment(getTobeHostedFragment(selectedCategory));
+
+
+        FloatingActionButton fab =findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
+    }
+
+    private void addFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.detail_container,fragment);
+        fragmentTransaction.commitNow();
+    }
+
+    private void setHeaderImage(int selectedCategory)
+    {
+        switch (selectedCategory)
+        {
+            case 0:
+                headerImageView.setImageResource(R.drawable.schedule2);
+                break;
+            case 1:
+                headerImageView.setImageResource(R.drawable.attendence);
+                break;
+            case 2:
+                headerImageView.setImageResource(R.drawable.marks);
+                break;
+            case 3:
+                headerImageView.setImageResource(R.drawable.reminder);
+                break;
+        }
+    }
+
+    private Fragment getTobeHostedFragment(int category)
+    {
+        switch (category)
+        {
+            case 0:
+                return OverviewFragment.newInstance();
+            case 1:
+                return OverviewFragment.newInstance();
+            case 2:
+                return OverviewFragment.newInstance();
+            case 3:
+                return OverviewFragment.newInstance();
+        }
+        return null;
+    }
+}
--- app/src/main/java/drunkcoder/com/collegebuddy/OverviewFragment.java
+++ app/src/main/java/drunkcoder/com/collegebuddy/OverviewFragment.java
@@ -1,9 +1,12 @@
 package drunkcoder.com.collegebuddy;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +16,16 @@ import androidx.fragment.app.Fragment;
  * FRAGMENT DISPLAYS THE OVERVIEW OF THE USER SCHEDUAL, ATTENDANCE , MARKS  AND REMINDERS
  *
  */
-public class OverviewFragment extends Fragment {
+public class OverviewFragment extends Fragment implements View.OnClickListener {
+
+
+    private TextView mCardDetailTextView1;
+    private TextView mCardDetailTextView2;
+    private TextView mCardDetailTextView3;
+    private TextView mCardDetailTextView4;
+
+    private Activity hostingActivity;
+
 
     @Nullable
     @Override
@@ -21,11 +33,55 @@ public class OverviewFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_overview,container,false);
 
+        mCardDetailTextView1=view.findViewById(R.id.card_detail1);
+        mCardDetailTextView2=view.findViewById(R.id.card_detail2);
+        mCardDetailTextView3=view.findViewById(R.id.card_detail3);
+        mCardDetailTextView4=view.findViewById(R.id.card_detail4);
+        mCardDetailTextView1.setOnClickListener(this);
+        mCardDetailTextView2.setOnClickListener(this);
+        mCardDetailTextView3.setOnClickListener(this);
+        mCardDetailTextView4.setOnClickListener(this);
+
+        hostingActivity=getActivity();
+
+
+
+
         return view;
     }
 
     public static OverviewFragment newInstance()
     {
         return new OverviewFragment();
+    }
+
+
+    // handle click events
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.card_detail1:
+                moveToDetailsActivity(0);
+                break;
+            case R.id.card_detail2:
+                moveToDetailsActivity(1);
+                break;
+            case R.id.card_detail3:
+                moveToDetailsActivity(2);
+                break;
+            case R.id.card_detail4:
+                moveToDetailsActivity(3);
+                break;
+        }
+
+    }
+
+    private void moveToDetailsActivity(int category)
+    {
+        Intent intent = new Intent(hostingActivity,DetailsActivity.class);
+        intent.putExtra("selectedCategory",category);
+        startActivity(intent);
     }
 }
